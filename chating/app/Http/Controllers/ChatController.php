33e437792e4ab9fc77596eebea9a0ae1ca3804c6
nesta <nestatbn28@gmail.com>
2021-11-25 14:34:00 +0700

    Membuat API untuk melihat seluruh percakapan

--- chating/app/Http/Controllers/ChatController.php
+++ chating/app/Http/Controllers/ChatController.php
@@ -33,7 +33,11 @@ class ChatController extends Controller
         $daftar =  DB::table('user as pen')->join('chat', 'chat.penerima', '=', 'pen.id')
         ->join('user as peng', 'chat.pengirim', '=', 'peng.id')
         ->select('chat.message','pen.nama as penerima')
-        ->where('id','=',3)
+        ->where('pengirim','=',$id)
+        ->where('penerima','=',$tujuan)
+        ->orWhere('pengirim','=',$tujuan)
+        ->where('penerima','=',$id)
+        ->orderBy('created_at')
         ->get();
         return response()->json([
             "success" => true,
@@ -64,48 +68,6 @@ class ChatController extends Controller
       }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Chat  $chat
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Chat $chat)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Chat  $chat
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Chat $chat)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Chat  $chat
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Chat $chat)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Chat  $chat
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Chat $chat)
-    {
-        //
-    }
+    
 }
